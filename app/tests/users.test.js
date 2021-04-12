ddf38a9bf19489aafd08a1f16af3b4d68e7b38c4
ciprian.vacaru <ciprian.vacaru@fortech.ro>
2021-04-12 10:28:31 +0300

    add more user test

--- app/tests/users.test.js
+++ app/tests/users.test.js
@@ -1,30 +1,32 @@
 const chai = require("chai");
 const should = chai.should();
 const expect = chai.expect;
+const bcrypt = require("bcrypt");
+
 const chaiHttp = require("chai-http");
 const app = require("../index");
 const db = require("../models");
 
 chai.use(chaiHttp);
 
-// db.sequelize.sync({ force: true }).then(function () {
-//   done();
-// });
-
 const usersUrl = "/api/v1/users";
+
 describe("Users route", () => {
   beforeEach(async () => {
+    const salt = await bcrypt.genSalt();
+    const hashedPassword = await bcrypt.hash("pass123", salt);
+
     await db.sequelize.sync({ force: true });
     await db.User.create({
       username: "name",
-      password: "pass22",
+      password: hashedPassword,
       age: 23,
       sex: "male",
       email: "email@email.com",
     });
     await db.User.create({
       username: "name2",
-      password: "pass22",
+      password: hashedPassword,
       age: 28,
       sex: "female",
       email: "email@email2.com",
@@ -78,9 +80,30 @@ describe("Users route", () => {
       throw new Error("error");
     }
   });
+
+  it("should login with success ", async () => {
+    const body = {
+      username: "name",
+      password: "pass123",
+    };
+    try {
+      const response = await chai
+        .request(app)
+        .post(`${usersUrl}/login`)
+        .send(body);
+
+      const { success } = await response.body;
+
+      expect(response.status).to.equal(200);
+      expect(success).to.be.true;
+      expect(response.body).to.have.property("token");
+    } catch (error) {
+      throw new Error("error");
+    }
+  });
   it("should get all the users on success", async () => {
     try {
-      const response = await chai.request(app).get("/api/v1/users");
+      const response = await chai.request(app).get(usersUrl);
       const { users } = await response.body;
 
       expect(response.status).to.equal(200);
@@ -114,12 +137,43 @@ describe("Users route", () => {
 
   it("should get a single user record", async () => {
     const id = 1;
-
     try {
       const response = await chai.request(app).get(`${usersUrl}/${id}`);
 
       expect(response.status).to.equal(200);
       expect(response.body).to.be.a("object");
+    } catch (error) {
+      throw new Error("error");
+    }
+  });
+
+  it("it should update a user given an id", async () => {
+    const id = 1;
+    try {
+      const response = await chai.request(app).put(`${usersUrl}/${id}`).send({
+        email: "john@email.com",
+      });
+
+      expect(response.status).to.equal(200);
+      expect(response.body).to.be.a("object");
+
+      const responseUser = await chai.request(app).get(`${usersUrl}/${id}`);
+      expect(responseUser.body).to.have.property("email").eql("john@email.com");
+    } catch (error) {
+      throw new Error("error");
+    }
+  });
+
+  it("it should delete a user given an id", async () => {
+    const id = 1;
+    try {
+      const response = await chai.request(app).delete(`${usersUrl}/${id}`);
+
+      expect(response.status).to.equal(200);
+      expect(response.body).to.be.a("object");
+
+      const responseUser = await chai.request(app).get(`${usersUrl}/${id}`);
+      expect(responseUser.status).to.equal(401);
     } catch (error) {
       throw new Error("error");
     }
